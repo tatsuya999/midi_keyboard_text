2de892e78f75f3b6845a9dc4ded070e27350a757
tatsuya_ego <ts2000kasa@gmail.com>
2021-09-08 11:30:28 +0900

    コメントアウトを修正

--- KeyboardMidi.py
+++ KeyboardMidi.py
@@ -8,14 +8,14 @@ key_action = ['space','enter','return','backspace','esc','command','shift']
 key_text = [['q','a','z'],['w','s','x'],['e','d','c'],['r','f','v','t','g','b'],['y','h','n','u','j','m'],['i','k',','],['o','l','.'],['p',';','/']]
 #音階の名前リスト
 scale_name = ['C','C#','D','Eb','E','F','F#','G','G#','A','Bb','B']
-#音階の位置
+#音階の位置リスト
 scale_list_mold = np.zeros((11,12))
 for i in range(12):
     for n in range(11):
         scale_list_mold[n][i]=i+12*n
 scale_list = scale_list_mold.T
 
-#音階の位置を探索する関数
+#音階の位置を探索する関数(リスト,音階位置を引数)ドレミファを返す
 def return_scale_num(list_s,search_num):
     return_num = 0
     for i in range(12):
@@ -37,8 +37,9 @@ except IndexError:
 
 try:
     while True:
-        # [状態(押した/離した), 押したキーの場所, 押した強さ]
         message = input_port.get_message()
+        #message[状態(押した144/離した128), 押したキーの場所, 押した強さ]
+        #message[1]== 0->C,1->C#,.....11->B,13->C
         if message:
             if message[0]==144:
                 key_num = return_scale_num(scale_list,message[1])
